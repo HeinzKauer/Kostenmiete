8fd1850645dcd5324d581670ea00fd88ec6daad9
Heinz Kauer <heinz.kauer@gmx.net>
2013-10-02 00:27:09 +0200

    Javadoc

--- src/ch/kausoft/wbg/daten/Daten.java
+++ src/ch/kausoft/wbg/daten/Daten.java
@@ -8,7 +8,7 @@ public class Daten {
   String bezeichnung;
 
   /**
-   * 
+   * Beschreibung 
    */
   String beschreibung;
 
--- src/ch/kausoft/wbg/daten/Investition.java
+++ src/ch/kausoft/wbg/daten/Investition.java
@@ -10,81 +10,81 @@ import java.util.HashMap;
  */
 public class Investition extends Daten {
 
-   /**
+	/**
+	 * Ein investierter Betrag der während einer bestimmten Dauer amortisiert
+	 * und getilgt werden soll
+	 */
+	int investitionsBetrag;
+
+	/**
+	 * Ein Teil der Investition kann aus Rückstellungen getilgt werden.
+	 */
+	int investitionAusRueckstellung;
+
+	/**
+	 * Differenz investitionsBetrag - investitionAusRueckstellung
+	 */
+	int investitionZumTilgen;
+
+	/**
+	 * Aktiviwreungsjahr
+	 */
+	int aktivierungsjahr;
+
+	/**
    * 
    */
-   int investitionsBetrag;
+	int lebensdauerInJahre;
 
-   /**
-   * 
-   */
-   int investitionAusRueckstellung;
-
-   /**
-   * 
-   */
-   int investitionZumTilgen;
-
-   /**
-   * 
-   */
-   int aktivierungsjahr;
-
-   /**
-   * 
-   */
-   int lebensdauerInJahre;
-
-   /**
+	/**
     * 
     */
-   public double tilgungProWohnung[] = new double[14];
-   
-   
-   public Tilgung tilgung;
+	public double tilgungProWohnung[] = new double[14];
 
-   /**
-    * Wird Amortisiert vom Kapital
-    */
-   Kapital kapital;
+	public Tilgung tilgung;
 
-   HashMap<Integer, Mietzinsbeitrag> mietzinsbeitrag = new HashMap<Integer, Mietzinsbeitrag>();
+	/**
+	 * Wird Amortisiert vom Kapital
+	 */
+	Kapital kapital;
 
-   public Investition(String bezeichnung) {
-      super(bezeichnung, "");
-   }
+	HashMap<Integer, Mietzinsbeitrag> mietzinsbeitrag = new HashMap<Integer, Mietzinsbeitrag>();
 
-   public Investition(String bezeichnung, String beschreibung,
-         int investitionsBetrag, int investitionAusRueckstellung,
-         int aktivierungsjahr, int lebensdauerInJahre, Kapital kapital) {
+	public Investition(String bezeichnung) {
+		super(bezeichnung, "");
+	}
 
-      super(bezeichnung, beschreibung);
+	public Investition(String bezeichnung, String beschreibung,
+			int investitionsBetrag, int investitionAusRueckstellung,
+			int aktivierungsjahr, int lebensdauerInJahre, Kapital kapital) {
 
-      this.investitionsBetrag = investitionsBetrag;
-      this.investitionAusRueckstellung = investitionAusRueckstellung;
-      this.aktivierungsjahr = aktivierungsjahr;
-      this.lebensdauerInJahre = lebensdauerInJahre;
-      this.investitionZumTilgen = investitionsBetrag
-            - investitionAusRueckstellung;
-      this.kapital = kapital;
-   }
+		super(bezeichnung, beschreibung);
 
-   public Zinssatz getKapitalZins(int jahr) {
-      return kapital.getKapitalZins(jahr);
-   }
+		this.investitionsBetrag = investitionsBetrag;
+		this.investitionAusRueckstellung = investitionAusRueckstellung;
+		this.aktivierungsjahr = aktivierungsjahr;
+		this.lebensdauerInJahre = lebensdauerInJahre;
+		this.investitionZumTilgen = investitionsBetrag
+				- investitionAusRueckstellung;
+		this.kapital = kapital;
+	}
 
-   public Mietzinsbeitrag getMietzinsbeitrag(int jahr) {
-      for (int i = jahr; i >= 2000; i--) {
-         Mietzinsbeitrag x = mietzinsbeitrag.get(i);
-         if (x != null) {
-            return x;
-         }
-      }
-      return null;
-   }
+	public Zinssatz getKapitalZins(int jahr) {
+		return kapital.getKapitalZins(jahr);
+	}
 
-   public void addMietzinsbeitrag(Mietzinsbeitrag mietzinsbeitrag) {
-      mietzinsbeitrag.invest = this;
-      this.mietzinsbeitrag.put(mietzinsbeitrag.abJahr, mietzinsbeitrag);
-   }
+	public Mietzinsbeitrag getMietzinsbeitrag(int jahr) {
+		for (int i = jahr; i >= 2000; i--) {
+			Mietzinsbeitrag x = mietzinsbeitrag.get(i);
+			if (x != null) {
+				return x;
+			}
+		}
+		return null;
+	}
+
+	public void addMietzinsbeitrag(Mietzinsbeitrag mietzinsbeitrag) {
+		mietzinsbeitrag.invest = this;
+		this.mietzinsbeitrag.put(mietzinsbeitrag.abJahr, mietzinsbeitrag);
+	}
 }
