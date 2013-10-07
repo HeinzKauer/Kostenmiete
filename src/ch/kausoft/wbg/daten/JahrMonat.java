912ea28ecf95eac8050e7f9e879fe6432ac169d9
Heinz Kauer <heinz.kauer@gmx.net>
2013-10-07 22:58:55 +0200

    Neue Classe Jahr Monat

---
+++ src/ch/kausoft/wbg/daten/JahrMonat.java
@@ -0,0 +1,12 @@
+package ch.kausoft.wbg.daten;
+
+public class JahrMonat {
+
+	private int jahr, monat;
+
+	public JahrMonat(int jahr, int monat) {
+		this.jahr = jahr;
+		this.monat = monat;
+	}
+
+}
